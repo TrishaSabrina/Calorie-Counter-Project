68ad25b1de91940f133ad84e6a4d4c0b9ef51ad7
Trisha <johndoe@example.com>
2024-09-09 21:26:29 +0600

    ch

--- src/script.js
+++ src/script.js
@@ -18,9 +18,12 @@ function cleanInputString(str) {
     return str.match(regex);
   }
  
-
   function addEntry() {
-    const targetId = '#' + entryDropdown.value;
-    const targetInputContainer=document.querySelector(targetId+' .input-container' );
-  
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
   }
