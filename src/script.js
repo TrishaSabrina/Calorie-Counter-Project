1118425b7e593d50bc169961b7e87f1b021d8da3
Trisha <johndoe@example.com>
2024-09-11 01:50:31 +0600

    s

--- src/script.js
+++ src/script.js
@@ -49,6 +49,10 @@ function cleanInputString(str) {
     if(isError){
       return
     }
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories= budgetCalories-consumedCalories+exerciseCalories;
+    const surplusOrDeficit=remainingCalories <0 ? "Surplus":"Deficit" ;
+    output.innerHTML =``;
 
   }
 
